5a39352351727a41be3ff8fae76d8b2436ecfd83
Kenier Becerra Becerra <kenier.becerra@siigroup.cl>
2025-10-14 09:00:33 -0300

    cambiar logo en navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Logo from "../../assets/Logo.png";
+import Logo from "../../assets/blanco.png";
 
 const NAV_LINKS = [
   { id: 1, title: "Inicio", href: "#inicio" },
